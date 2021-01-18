60c7250eafeb9bc5417eabfbe9a41180326c47e4
Valeria Orlova <v.orlova@sprinteins.com>
2021-01-18 15:49:57 +0100

    add DrupalClientApplication class base

---
+++ src/main/java/com/sprinteins/drupalcli/DrupalClientApplication.java
@@ -0,0 +1,9 @@
+package com.sprinteins.drupalcli;
+
+public class DrupalClientApplication {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hi there!");
+    }
+    
+}
