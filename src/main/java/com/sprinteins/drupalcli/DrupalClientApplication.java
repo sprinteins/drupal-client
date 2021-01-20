f91b1bb458790af9c28dc9bc31e881ee1085e058
Arlo O'Keeffe <tiger@k5d.de>
2021-01-20 15:54:46 +0100

    output json in application

--- src/main/java/com/sprinteins/drupalcli/DrupalClientApplication.java
+++ src/main/java/com/sprinteins/drupalcli/DrupalClientApplication.java
@@ -1,9 +1,21 @@
 package com.sprinteins.drupalcli;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class DrupalClientApplication {
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hi there!");
-    }
-    
+	public static void main(String[] args) throws Exception {
+		System.out.println("Hi there!");
+
+		GetStartedParagraphModel getStartedParagraph = new GetStartedParagraphModel();
+		FieldDescriptionModel fieldDescription = getStartedParagraph
+				.getOrCreateFirstDescription();
+		fieldDescription.setFormat(ValueFormat.GITHUB_FLAVORED_MARKDOWN);
+		fieldDescription.setValue("# Title");
+
+		String output = new ObjectMapper()
+				.writeValueAsString(getStartedParagraph);
+		System.out.println(output);
+	}
+
 }
