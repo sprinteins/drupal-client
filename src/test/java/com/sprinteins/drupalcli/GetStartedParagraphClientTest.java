f32417404c591fec5438a36065c54fa45ba31373
Valeria Orlova <v.orlova@sprinteins.com>
2021-01-22 09:37:56 +0100

    test: add successful patch test

---
+++ src/test/java/com/sprinteins/drupalcli/GetStartedParagraphClientTest.java
@@ -0,0 +1,31 @@
+package com.sprinteins.drupalcli;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class GetStartedParagraphClientTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void test() {
+        GetStartedParagraphModel getStartedParagraph = new GetStartedParagraphModel();
+
+        DescriptionModel fieldDescription = getStartedParagraph
+                .getOrCreateFirstDescription();
+        fieldDescription.setFormat(ValueFormat.GITHUB_FLAVORED_MARKDOWN);
+        fieldDescription.setValue("Test Value");
+        try {
+            new GetStartedParagraphClient("http://localhost:"+port+"/entity/paragraph/")
+                    .patch(195, getStartedParagraph);
+        } catch(Exception e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+            Assertions.fail();
+        }
+    }
+}
