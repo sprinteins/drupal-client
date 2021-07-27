6630f71295defe301b4ce9f5f8b9ee286a02839d
Philipp Wiegand <p.wiegand@sprinteins.com>
2021-07-27 11:14:26 +0200

    feat: add download for images and api reference yaml
    feat: add release notes to export

--- src/main/java/com/sprinteins/drupalcli/file/ApiReferenceFileClient.java
+++ src/main/java/com/sprinteins/drupalcli/file/ApiReferenceFileClient.java
@@ -15,17 +15,19 @@ public class ApiReferenceFileClient {
     private final ObjectMapper objectMapper;
     private final String baseUri;
     private final String apiKey;
+    private final String uploadBaseUri;
 
     public ApiReferenceFileClient(ObjectMapper objectMapper, String baseUri, String apiKey) {
         this.objectMapper = objectMapper;
-        this.baseUri = baseUri + "/file/upload/node/api_reference/field_source_file";
+        this.uploadBaseUri = baseUri + "/file/upload/media/file/field_media_file";
+        this.baseUri = baseUri;
         this.apiKey = apiKey;
     }
 
     public FileUploadModel upload(Path path) {
         try {
             HttpRequest request = HttpRequestBuilderFactory
-                    .create(URI.create(baseUri + "?_format=json"), apiKey)
+                    .create(URI.create(uploadBaseUri + "?_format=json"), apiKey)
                     .POST(HttpRequest.BodyPublishers.ofFile(path))
                     .header("Content-Type", "application/octet-stream")
                     .header("Content-Disposition", "file; filename=\"" + path.getFileName() + "\"")
@@ -37,6 +39,22 @@ public class ApiReferenceFileClient {
             return objectMapper.readValue(httpResponse.body(), FileUploadModel.class);
         } catch (IOException | InterruptedException e) {
             throw new IllegalStateException("Upload failed", e);
+        }
+    }
+
+    public byte[] download(String link){
+        try {
+            HttpRequest request = HttpRequestBuilderFactory
+                    .create(URI.create(link), apiKey)
+                    .method("GET", HttpRequest.BodyPublishers.noBody())
+                    .build();
+
+            HttpResponse<byte[]> httpResponse = HttpClientBuilderFactory.create().build()
+                    .send(request, HttpResponse.BodyHandlers.ofByteArray());
+
+            return httpResponse.body();
+        } catch (IOException | InterruptedException e) {
+            throw new IllegalStateException("Download failed", e);
         }
     }
 
