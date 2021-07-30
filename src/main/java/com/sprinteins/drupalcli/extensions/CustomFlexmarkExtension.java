35062a5848c11346cfe347cf08e537b1ab4d768a
Arlo O'Keeffe <tiger@k5d.de>
2021-07-30 15:29:58 +0200

    Remove custom LinkResolver

--- src/main/java/com/sprinteins/drupalcli/extensions/CustomFlexmarkExtension.java
+++ src/main/java/com/sprinteins/drupalcli/extensions/CustomFlexmarkExtension.java
@@ -1,14 +1,11 @@
 package com.sprinteins.drupalcli.extensions;
 
-import com.vladsch.flexmark.html.renderer.ResolvedLink;
 import com.vladsch.flexmark.html2md.converter.*;
 import com.vladsch.flexmark.html2md.converter.internal.HtmlConverterCoreNodeRenderer;
 import com.vladsch.flexmark.util.data.DataHolder;
 import com.vladsch.flexmark.util.data.MutableDataHolder;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
 
 import java.util.HashSet;
 import java.util.List;
@@ -28,46 +25,7 @@ public class CustomFlexmarkExtension {
 
         @Override
         public void extend(FlexmarkHtmlConverter.@NotNull Builder builder) {
-            builder.linkResolverFactory(new CustomLinkResolver.Factory());
             builder.htmlNodeRendererFactory(new CustomTagConverter.Factory());
-        }
-    }
-
-    static class CustomLinkResolver implements HtmlLinkResolver {
-        public CustomLinkResolver(HtmlNodeConverterContext context) {
-        }
-
-        @Override
-        public ResolvedLink resolveLink(Node node, HtmlNodeConverterContext context, ResolvedLink link) {
-            // convert all links from http:// to https://
-            if (link.getUrl().startsWith("http:")) {
-                return link.withUrl("https:" + link.getUrl().substring("http:".length()));
-            }
-            return link;
-        }
-
-        static class Factory implements HtmlLinkResolverFactory {
-            @Nullable
-            @Override
-            public Set<Class<?>> getAfterDependents() {
-                return null;
-            }
-
-            @Nullable
-            @Override
-            public Set<Class<?>> getBeforeDependents() {
-                return null;
-            }
-
-            @Override
-            public boolean affectsGlobalScope() {
-                return false;
-            }
-
-            @Override
-            public HtmlLinkResolver apply(HtmlNodeConverterContext context) {
-                return new CustomLinkResolver(context);
-            }
         }
     }
 
