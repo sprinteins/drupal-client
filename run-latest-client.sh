2ec35c83a80ce2cc1ed19dd3e5a85a3e4104c090
Arlo O'Keeffe <tiger@k5d.de>
2021-09-16 13:42:52 +0200

    Add run-latest-client.sh script

---
+++ run-latest-client.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+#set -o xtrace
+
+OS="$(uname)"
+if [ "${OS}" = "Linux" ]; then
+  	platform=linux
+elif [ "${OS}" = "Darwin" ]; then
+  	platform=macos
+else
+	platform=windows
+fi
+
+cleanup() {
+    currentExitCode=$?
+    rm -f "./drupal-client"
+    exit ${currentExitCode}
+}
+
+trap cleanup INT TERM EXIT
+
+wget --quiet \
+    --output-document="./drupal-client" \
+    "https://github.com/sprinteins/drupal-client/releases/latest/download/drupal-client-${platform}"
+
+chmod +x "./drupal-client"
+
+"./drupal-client" --version
+
+"./drupal-client" "${@}"
