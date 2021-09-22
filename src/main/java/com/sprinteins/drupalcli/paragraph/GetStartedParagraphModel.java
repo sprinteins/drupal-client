39abd416eb83d9f8d5ba447c9bb54fca30c5a33f
Arlo O'Keeffe <tiger@k5d.de>
2021-09-22 15:35:09 +0200

    Add convenience constructor

--- src/main/java/com/sprinteins/drupalcli/paragraph/GetStartedParagraphModel.java
+++ src/main/java/com/sprinteins/drupalcli/paragraph/GetStartedParagraphModel.java
@@ -14,6 +14,12 @@ public class GetStartedParagraphModel extends ParagraphModel{
     public GetStartedParagraphModel(){
         super(new TypeModel(TargetId.GET_STARTED_ELEMENT));
     }
+    
+    public GetStartedParagraphModel(String title, DescriptionModel description) {
+        this();
+        getOrCreateFirstTitle().setValue(title);
+        setDescription(List.of(description));
+    }
 
     @Override
     @JsonProperty("field_p_getstarted_title")
