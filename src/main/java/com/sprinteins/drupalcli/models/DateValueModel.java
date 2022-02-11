795f3c4810ebd1950a1176a2198ddabbe7942468
Philipp Pfeil <p.wiegand@sprinteins.com>
2022-02-11 09:09:41 +0100

    add validation pattern to DateValueModel

--- src/main/java/com/sprinteins/drupalcli/models/DateValueModel.java
+++ src/main/java/com/sprinteins/drupalcli/models/DateValueModel.java
@@ -1,8 +1,12 @@
 package com.sprinteins.drupalcli.models;
 
+import javax.validation.constraints.Pattern;
+
 public class DateValueModel {
 
+    @Pattern(regexp="^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$", message="Value for date is not valid, please use YYYY-MM-DD\n")
     private String value;
+
     private String format;
 
     public String getValue() {
