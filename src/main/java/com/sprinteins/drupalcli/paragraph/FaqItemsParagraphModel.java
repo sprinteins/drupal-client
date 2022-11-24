55fd86e43cb1bf38b8626b9c874d9e6f94777238
Rutul Maskar (Corporate IT Office), external <rutul.maskar@dpdhl.com>
2022-11-24 17:44:46 +0100

    update faqItemsParagraphModel

--- src/main/java/com/sprinteins/drupalcli/paragraph/FaqItemsParagraphModel.java
+++ src/main/java/com/sprinteins/drupalcli/paragraph/FaqItemsParagraphModel.java
@@ -2,13 +2,17 @@ package com.sprinteins.drupalcli.paragraph;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sprinteins.drupalcli.models.FaqItemModel;
 import com.sprinteins.drupalcli.models.TargetId;
 import com.sprinteins.drupalcli.models.TitleModel;
 import com.sprinteins.drupalcli.models.TypeModel;
 
 public class FaqItemsParagraphModel extends ParagraphModel {
+
+  private List<FaqItemModel> faqItem;
 
   public static FaqItemsParagraphModel create(String title) {
     FaqItemsParagraphModel model = new FaqItemsParagraphModel();
@@ -19,6 +23,14 @@ public class FaqItemsParagraphModel extends ParagraphModel {
   public FaqItemsParagraphModel() {
     super(new TypeModel(TargetId.FAQ));
   }
+
+  @JsonProperty("field_faq_item")
+  public List<FaqItemModel> getFaqItem() {
+      return Optional.ofNullable(faqItem).map(List::copyOf).orElse(null);
+  }
+  public void setFaqItem(List<FaqItemModel> faqItem) {
+      this.faqItem = Optional.ofNullable(faqItem).map(List::copyOf).orElse(null);
+  } 
 
   @Override
   @JsonProperty("field_faq_title")
