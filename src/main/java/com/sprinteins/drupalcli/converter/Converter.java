1f50f39c7d3286b6ac1ab1483ddf33a822d9cf8f
Leo Hein (Corporate IT Office), external <leo.hein@dpdhl.com>
2022-12-08 13:50:30 +0100

    Added carriage return and line feed back to htmlToMarkdown converter.

--- src/main/java/com/sprinteins/drupalcli/converter/Converter.java
+++ src/main/java/com/sprinteins/drupalcli/converter/Converter.java
@@ -39,9 +39,6 @@ public class Converter {
         // microsoft word aka long dash is replaced with regular minus
         input = input.replace("–","-");
         input = input.replace("&nbsp;", " ");
-        input = input.replaceAll("\r", " ");
-        input = input.replaceAll("\n", " ");
-
 
         Document document = Jsoup.parse(input, link);
         Safelist safelist = Safelist.relaxed();
