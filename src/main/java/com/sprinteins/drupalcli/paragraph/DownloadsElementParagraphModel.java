df05abee25c9ec301e7a40ef0910d31a151c2d0a
Rutul Maskar (Corporate IT Office), external <rutul.maskar@dpdhl.com>
2023-01-05 14:56:51 +0100

    create paragraph model for Downloads

---
+++ src/main/java/com/sprinteins/drupalcli/paragraph/DownloadsElementParagraphModel.java
@@ -0,0 +1,23 @@
+package com.sprinteins.drupalcli.paragraph;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sprinteins.drupalcli.fields.SourceFileModel;
+import com.sprinteins.drupalcli.fields.TypeModel;
+import com.sprinteins.drupalcli.fieldtypes.TargetId;
+
+public class DownloadsElementParagraphModel extends ParagraphModel {
+
+  private List<SourceFileModel> downloadableFiles;
+
+  public DownloadsElementParagraphModel() {
+    super(new TypeModel(TargetId.DOWNLOAD_ELEMENT));
+  }
+
+  @JsonProperty("field_file_to_upload")
+  public List<SourceFileModel> getDownloadFiles() {
+    return Optional.ofNullable(downloadableFiles).map(List::copyOf).orElse(null);
+  }
+}
