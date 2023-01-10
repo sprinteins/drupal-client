cc7402e9e1ed768e6922136118f098e20dc4443c
Rutul Maskar (Corporate IT Office), external <rutul.maskar@dpdhl.com>
2023-01-10 20:17:48 +0100

    fix uploadBaseUri

--- src/main/java/com/sprinteins/drupalcli/file/DownloadFileClient.java
+++ src/main/java/com/sprinteins/drupalcli/file/DownloadFileClient.java
@@ -12,7 +12,7 @@ public class DownloadFileClient extends FileClient {
 
 	public DownloadFileClient(ObjectMapper objectMapper, String baseUri, String apiKey, HttpClient httpClient) {
 		super(objectMapper, baseUri, apiKey, httpClient);
-		this.uploadBaseUri = baseUri + "/file/upload/paragraph/downloads/field_file_to_upload?_format=json";
+		this.uploadBaseUri = baseUri + "/file/upload/paragraph/downloads/field_file_to_upload";
 	}
 
 	public FileUploadModel upload(Path path) throws NoSuchAlgorithmException {
