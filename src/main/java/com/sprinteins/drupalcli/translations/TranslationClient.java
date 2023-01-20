5c98dc2f4507800860c1efa184deb1b88145dbd0
Rutul Maskar (Corporate IT Office), external <rutul.maskar@dpdhl.com>
2023-01-20 13:42:14 +0100

    create request to get-translations endpoint

---
+++ src/main/java/com/sprinteins/drupalcli/translations/TranslationClient.java
@@ -0,0 +1,44 @@
+package com.sprinteins.drupalcli.translations;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sprinteins.drupalcli.HttpRequestBuilderFactory;
+import com.sprinteins.drupalcli.HttpResponseStatusHandler;
+
+public class TranslationClient {
+  private ObjectMapper objectMapper;
+  private String baseUri;
+  private String apiKey;
+  private HttpClient httpClient;
+
+  public TranslationClient(ObjectMapper objectMapper, String baseUri, String apiKey, HttpClient httpClient) {
+        this.objectMapper = objectMapper;
+        this.baseUri = baseUri;
+        this.apiKey = apiKey;
+        this.httpClient = httpClient;
+    }
+
+  public TranslationModel getTranslations(long nid) {
+    try {
+      HttpRequest request = HttpRequestBuilderFactory
+              .create(URI.create(baseUri + "/get-translations/" + nid + "?_format=json"), apiKey)
+              .GET()
+              .header("Content-Type", "application/json")
+              .build();
+
+      HttpResponse<String> httpResponse = httpClient
+              .send(request, HttpResponse.BodyHandlers.ofString());
+
+      HttpResponseStatusHandler.checkStatusCode(httpResponse);
+
+      return objectMapper.readValue(httpResponse.body(), TranslationModel.class);
+    } catch (IOException | InterruptedException e) {
+      throw new IllegalStateException("Get translations failed", e);
+    }
+  }
+}
--- src/main/java/com/sprinteins/drupalcli/translations/TranslationModel.java
+++ src/main/java/com/sprinteins/drupalcli/translations/TranslationModel.java
@@ -3,6 +3,7 @@ package com.sprinteins.drupalcli.translations;
 import java.util.List;
 import java.util.Optional;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sprinteins.drupalcli.fieldtypes.StringValueModel;
 
 public class TranslationModel {
@@ -10,7 +11,7 @@ public class TranslationModel {
   private List<StringValueModel> title;
 
   public TranslationModel() {}
-
+      @JsonProperty("title")
       public List<StringValueModel> getTitle() {
         return Optional.ofNullable(title).map(List::copyOf).orElse(null);
     }
